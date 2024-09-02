225cb2156b9ccd9ea0ae99b1e801d363d5ded3e2
Chris Villarroel <chrisdev3001@gmail.com>
2024-09-02 08:22:39 -0400

    se integra conexion con BD postgreSQL

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Koa from 'koa'
 import Router from 'koa-router'
 import { setFinalResponseMdw, setResponseTimeMdw } from './middlewares.js'
+import { UserRepository } from './database/UserRepository.js'
 
 const app = new Koa()
 const router = new Router()
@@ -12,8 +13,9 @@ app
   .use(router.routes())
   .use(router.allowedMethods())
 
-router.get('/user', (ctx, next) => {
-  ctx.body = { ok: true, message: 'Hola desde GET' }
+router.get('/user', async (ctx, next) => {
+  const responseDB = await UserRepository.getUsers()
+  ctx.body = { ok: true, message: responseDB }
 })
 
 router.post('/user', (ctx, next) => {
