ffcd3f77ad11a277e7b22e4f51f8a521d5b16646
Chris Villarroel <chrisdev3001@gmail.com>
2024-09-04 11:21:09 -0400

    se agrega mdw de validacion y de control de errores

--- src/userRouter.js
+++ src/userRouter.js
@@ -2,11 +2,12 @@ import Router from 'koa-router'
 import { userCtr } from './userCtr.js'
 import { loginCtr } from './loginCtr.js'
 import { validateTokenMiddleware } from '../middlewares.js'
+import { validateUpdateUserMdw } from './validateUpdateUserMdw.js'
 
 const router = new Router()
 
 router.post('/login', loginCtr.signIn)
-router.post('/user', userCtr.createUser)
+router.post('/user', validateUpdateUserMdw, userCtr.createUser)
 
 router.get('/user', validateTokenMiddleware, userCtr.getAllUsers)
 router.get('/user/:id', validateTokenMiddleware, userCtr.getUserById)
